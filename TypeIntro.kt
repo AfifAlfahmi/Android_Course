627311feb1691396c46c0ee46b9bb8a9d33189bf
AfifAlfahmi <72000582+AfifAlfahmi@users.noreply.github.com>
2020-09-28 08:02:05 +0300

    ch2 Variables

---
+++ TypeIntro.kt
@@ -0,0 +1,59 @@
+
+
+const val MAX_EXPERIENCE: Int = 5000
+fun main() {
+
+   //   Listing
+    var playerName: String = "Estragon"
+    var experiencePoints: Int = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+
+//   Challenge: hasSteed
+
+    var hasSteed=false
+        println(hasSteed)
+
+    // Challenge: The Unicorn’s Horn
+
+   val pubName="the Unicorn’s Horn"
+    val currentPubName="pubDuty"
+    var numOfGold=50
+    val drinks = listOf("soda","tea","LaCroix")
+
+
+
+
+    println("Do you need to stable a\n" +"steed?")
+    if(hasSteed == false){
+        println(" I have no steed. But I do have $numOfGold gold pieces, and I would like a drink" )
+       println( " $currentPubName : I have $drinks What will you have?")
+
+
+
+        // Challenge: Magic Mirror
+
+        var str = "Estragon"
+
+        var reverse = str.reversed()
+        println(reverse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
